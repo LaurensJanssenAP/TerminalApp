7a554f5dd5bb36b7b18dc89af7c5ffa0d4ccdc22
Laurens Janssen <laurens.janssen@ap.be>
2024-03-01 21:40:43 +0100

    test

--- consoleApp.ts
+++ consoleApp.ts
@@ -1,1 +1,5 @@
 import * as readline from "readline-sync";
+import personData from "./json/person.json";
+import countryDat from "./json/country.json";
+
+console.log("Welcome to the JSON data viewer!");
